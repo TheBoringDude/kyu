ff23b7210de4fae5c46986fe3bb673c594e3a9f4
TheBoringDude <iamcoderx@gmail.com>
2021-07-12 21:00:19 +0800

    feat: first working app, with basic function of query

---
+++ lib/src/qod/widget.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:kyu/src/qod/model.dart';
+import 'package:kyu/src/qod/qod.dart';
+
+class QODWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _QODWidgetState();
+}
+
+class _QODWidgetState extends State<QODWidget> {
+  late Future<QuoteOfTheDay> qod;
+
+  @override
+  void initState() {
+    super.initState();
+    qod = getQuoteOfTheDay();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<QuoteOfTheDay>(
+        future: qod,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return showQuote(snapshot.data);
+          } else if (snapshot.hasError) {
+            return Text('${snapshot.error}');
+          }
+
+          return CircularProgressIndicator();
+        });
+  }
+
+  Widget showQuote(QuoteOfTheDay? q) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 30),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            margin: EdgeInsets.only(bottom: 15),
+            child: Image.network("${q?.contents.quotes[0].background}", height: 150),
+          ),
+          Text(
+            "${q?.contents.quotes[0].quote}",
+            style: TextStyle(fontSize: 20, letterSpacing: 0.9),
+            textAlign: TextAlign.center,
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 10),
+            child: Text(
+              "- ${q?.contents.quotes[0].author}",
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 16),
+            ),
+          ),
+          Divider(height: 30),
+          Container(
+            margin: EdgeInsets.only(top: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [Text('They Said So®'), Text('\u00a9 ${q?.copyright.year}')],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
